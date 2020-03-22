603dfa30e3ee701f24b269dddaa4175b23c647c2
Coded Savage <54280221+Olasumbo-Eniola@users.noreply.github.com>
2020-03-22 17:16:56 +0100

    Add files via upload
    First JavaScript task

---
+++ script.js
@@ -0,0 +1,8 @@
+     var name = "Olasumbo Afolabi";
+    var courses = ["HTML", " CSS", " JavaScript"]
+    const data = [];
+    for (var i = 1; i < 200; i += 2) {
+         data.push(i)
+    
+    }
+    
